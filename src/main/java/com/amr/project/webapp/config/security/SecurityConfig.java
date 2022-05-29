84131f0561fab8de1b175f16ab15f59b585d5c30
RuslanSuleymanov <suli12345my@gmail.com>
2022-05-29 09:16:35 +0300

    implemented ReadWriteService in RestController

--- src/main/java/com/amr/project/webapp/config/security/SecurityConfig.java
+++ src/main/java/com/amr/project/webapp/config/security/SecurityConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@ComponentScan("com.amr.project")
-@EnableWebSecurity
+//@Configuration
+//@ComponentScan("com.amr.project")
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 }
