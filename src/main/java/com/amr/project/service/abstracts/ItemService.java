2da54d896fc28164922e99325a2d6de3cab2ff2a
Артем Орлов <arm.orlov@gmail.com>
2022-06-02 21:40:45 +0000

    Upload New File

---
+++ src/main/java/com/amr/project/service/abstracts/ItemService.java
@@ -0,0 +1,8 @@
+package com.amr.project.service.abstracts;
+
+import com.amr.project.model.entity.Item;
+import com.amr.project.model.entity.Shop;
+
+public interface ItemService extends ReadWriteService<Item,Long>{
+
+}
