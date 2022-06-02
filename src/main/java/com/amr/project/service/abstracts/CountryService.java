673f50c89d61b518f35e7dc75628a39282daaf03
Артем Орлов <arm.orlov@gmail.com>
2022-06-02 21:39:33 +0000

    Upload New File

---
+++ src/main/java/com/amr/project/service/abstracts/CountryService.java
@@ -0,0 +1,7 @@
+package com.amr.project.service.abstracts;
+
+import com.amr.project.model.entity.Country;
+import com.amr.project.model.entity.User;
+
+public interface CountryService extends ReadWriteService<Country,Long>{
+}
