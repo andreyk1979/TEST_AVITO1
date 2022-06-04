535dd021b71b56a0b03f2d071036ca2e93d27146
RuslanSuleymanov <suli12345my@gmail.com>
2022-06-04 08:30:34 +0300

    костыли

--- src/main/java/com/amr/project/webapp/controller/MainPageController.java
+++ src/main/java/com/amr/project/webapp/controller/MainPageController.java
@@ -1,7 +1,6 @@
 package com.amr.project.webapp.controller;
 
 import com.amr.project.service.abstracts.MainPageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
