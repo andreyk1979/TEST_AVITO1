727fa111a5a2e1b78b7b2dc434c62d08a3dc1906
mozAndrey <1852721@mail.ru>
2022-06-10 14:09:30 +0000

    Рест для 2fa

--- src/main/java/com/amr/project/dao/impl/UserDaoImp.java
+++ src/main/java/com/amr/project/dao/impl/UserDaoImp.java
@@ -6,4 +6,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDaoImp extends ReadWriteDaoImpl<User,Long> implements UserDao {
+    @Override
+    public User findByUserName(String username) {
+        return em.createQuery("select u from User u where u.username=:username",User.class)
+                .setParameter("username",username)
+                .getSingleResult();
+    }
 }
--- src/main/java/com/amr/project/service/abstracts/UserService.java
+++ src/main/java/com/amr/project/service/abstracts/UserService.java
@@ -3,4 +3,7 @@ package com.amr.project.service.abstracts;
 import com.amr.project.model.entity.User;
 
 public interface UserService extends ReadWriteService<User, Long>{
+    User findByUsername(String username);
+
+    boolean verifyUserBySecret(User user);
 }
