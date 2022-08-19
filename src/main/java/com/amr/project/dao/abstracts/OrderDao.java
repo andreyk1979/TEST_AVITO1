c229be2610ffc3511ed3f246cbe2808b3576aa24
Makeev Evgeny <makeevstranik@gmail.com>
2022-08-19 14:26:26 +0000

    #13131

--- src/main/java/com/amr/project/dao/abstracts/OrderDao.java
+++ src/main/java/com/amr/project/dao/abstracts/OrderDao.java
@@ -3,6 +3,9 @@ package com.amr.project.dao.abstracts;
 import com.amr.project.model.entity.Order;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDao extends  ReadWriteDao<Order,Long>{
+    List<Order> findAllNotActual();
 }
--- src/main/java/com/amr/project/service/impl/OrderServiceImpl.java
+++ src/main/java/com/amr/project/service/impl/OrderServiceImpl.java
@@ -1,24 +1,33 @@
 package com.amr.project.service.impl;
 
+import com.amr.project.dao.abstracts.OrderDao;
 import com.amr.project.dao.abstracts.ReadWriteDao;
 import com.amr.project.model.entity.Order;
 import com.amr.project.service.abstracts.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import java.util.List;
 
 
 @Service
 public class OrderServiceImpl extends ReadWriteServiceImpl<Order, Long> implements OrderService {
 
+    private final OrderDao orderDao;
 
     @Autowired
-    public OrderServiceImpl(@Qualifier("orderDaoImpl") ReadWriteDao<Order, Long> dao) {
+    public OrderServiceImpl(@Qualifier("orderDaoImpl") ReadWriteDao<Order, Long> dao, OrderDao orderDao) {
         super(dao);
+        this.orderDao = orderDao;
     }
 
+    @Transactional(readOnly = true)
     @Override
-    public Order findAllNotActual() {
-        return null;
+    public List<Order> findAllNotActual() {
+        return orderDao.findAllNotActual();
     }
 }
